af22cee2fcd723e4d30c3348cb9a2ab21ce3370f
isakf <isakf@uia.no>
2024-03-05 00:33:00 +0100

    Add project files.

---
+++ HICrew/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace HICrew
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ HICrew/LoginFormModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class LoginFormModel
+{
+    [Display(Prompt = "use example@mail.com", Name = "Email")]
+    public string Email { get; set; }
+
+    [Display(Prompt = "use 'demopassword'", Name = "Password")]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+}
