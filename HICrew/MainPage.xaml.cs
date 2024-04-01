a7e95c3943b3a770a457c8efbaf015cebadd8e7e
Isak Fagermo <isakf@uia.no>
2024-04-01 18:27:45 +0200

    Signup start

--- HICrew/MainPage.xaml.cs
+++ HICrew/MainPage.xaml.cs
@@ -1,7 +1,13 @@
-﻿namespace HICrew
+﻿using System.Windows.Input;
+
+namespace HICrew
 {
     public partial class MainPage : ContentPage
     {
+
+        public ICommand TapCommand => new Command<string>(async (url) => await Launcher.OpenAsync(url));
+
+
         public MainPage()
         {
             InitializeComponent();
