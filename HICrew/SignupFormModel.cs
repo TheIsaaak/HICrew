74a603e6f45eeb212c2858352ebed3ef17b31f23
Isak Fagermo <isakf@uia.no>
2024-04-09 21:40:42 +0200

    Signin stuff

--- HICrew/SignupFormModel.cs
+++ HICrew/SignupFormModel.cs
@@ -1,12 +1,16 @@
+using Syncfusion.Maui.Core;
+using Syncfusion.Maui.DataForm;
 using Syncfusion.Maui.Inputs;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-public class SignupFormModel
+
+    public class SignupFormModel
 {
     [Display(Name = "Firstname")]
     public string Firstname { get; set; }
@@ -14,23 +18,23 @@ public class SignupFormModel
     [Display(Name = "Lastname")]
     public string Lastname { get; set; }
 
+    [Display(Name = "Nationality")]
+    public string Country { get; set; }
+
     [Display(Name = "Date of birth")]
     public DateTime? BirthDate { get; set; }
-
-    [Display(Name = "Register as")]
-    public SfComboBox RegisterAs { get; set; }
-
+    
     [Display(Name = "Email")]
     public string Email { get; set; }
 
-    [Display(Prompt = "use example@mail.com", Name = "Confirm Email")]
+    [Display(Name = "Confirm Email")]
     public string ConfirmEmail { get; set; }
 
     [Display(Name = "Password")]
     [DataType(DataType.Password)]
     public string Password { get; set; }
 
-    [Display(Prompt = "use 'demopassword'", Name = "Confirm Password")]
+    [Display(Name = "Confirm Password")]
     [DataType(DataType.Password)]
     public string ConfirmPassword { get; set; }
 }
